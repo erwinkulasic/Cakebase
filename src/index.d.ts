feafdedff0e6b376e2547f52e6deb98cfa692f35
erwinkulasic <erwinkulasic@gmail.com>
2023-10-07 15:19:49 +0200

    Update for d.ts file added better description and more.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,41 +1,67 @@
 
-declare interface Features {
-    /**
-    * Insert an object.
-    * @param data the object.
-    */
-    set(data: object): Promise<void>;
+/**
+ * Defines a CakebaseObject for structured data.
+ * 
+ * The object can have any string keys, and the values must adhere to JSON-compatible 
+ * types, allowing for various data structures while ensuring type safety and JSON convertibility.
+ *
+ * @example
+ * const exampleObj: CakebaseObject = {
+ *   key1: "value",
+ *   key2: {
+ *     nestedKey: [true, null]
+ *   }
+ * };
+ */
+interface CakebaseObject {
+    [key: string]: string | number | boolean | CakebaseObject | CakebaseObject[] | null;
+}
 
+interface Functions {
     /**
-     * Retrieve for objects.
-     * @param prediciton the prediction to find the objects.
+     * Inserts an object into the database.
+     * @param data The object that is to be inserted.
+     * @returns A promise that resolves once the insertion is complete.
      */
-    get(prediciton: void): Promise<object[]>
+    set(data: CakebaseObject): Promise<void>;
 
     /**
-     * Remove objects that match the prediction.
-     * @param prediciton The prediction to find the objects.
+     * Retrieves objects from the database based on the provided predicate.
+     * @param predicate A function that determines whether an object should be retrieved.
+     * @returns A promise that resolves to an array of retrieved objects.
      */
-    remove(prediciton: void): Promise<void>;
+    get(predicate: (obj: CakebaseObject) => boolean): Promise<CakebaseObject[]>;
 
     /**
-     * Update objects that match the prediction.
-     * @param prediciton The prediction to find the objects.
-     * @param data Data you want to change or to insert.
+     * Removes objects from the database that satisfy the provided predicate.
+     * @param predicate A function that determines whether an object should be removed.
+     * @returns A promise that resolves once the removal is complete.
      */
-    update(prediciton: void, data: object): Promise<void>;
+    remove(predicate: (obj: CakebaseObject) => boolean): Promise<void>;
 
     /**
-     * Delete all objects.
+     * Updates objects in the database that satisfy the provided predicate.
+     * @param predicate A function that determines whether an object should be updated.
+     * @param data The new data that will be updated in the objects.
+     * @returns A promise that resolves once the update is complete.
+     */
+    update(predicate: (obj: CakebaseObject) => boolean, data: CakebaseObject): Promise<void>;
+
+    /**
+     * Deletes all objects from the database.
+     * @returns A promise that resolves once all objects are deleted.
      */
     clear(): Promise<void>;
 }
 
-
 /**
- * Initalize cakebase into your project.
- * @param path The path to your json file.
+ * Initializes Cakebase with a specified path to your JSON file.
+ * Cakebase provides a set of features for interacting with the database file, 
+ * such as inserting, retrieving, removing, and updating objects.
+ * 
+ * @param path A string representing the path to the JSON database file.
+ * @returns An object containing methods to interact with the database.
  */
-declare function cakebase(path: string): Features;
+declare function cakebase(path: string): Functions;
 
 export = cakebase;
